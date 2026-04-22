722fb80d8855f02e792cbb6757c5a5ceb116e385
Daniel Martin <dmartina@movistar.es>
2026-04-22 20:13:27 +0200

    Configurado MySQL 8 con Docker Compose

---
+++ sql/00_init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS ride_hailing
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_0900_ai_ci;
+
+USE ride_hailing;
+
+SELECT 'Base de datos ride_hailing creada correctamente' AS mensaje;
